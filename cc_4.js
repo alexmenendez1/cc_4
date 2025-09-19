ea8450ff845d687113d03c1c2c9790a7dd58c679
Alex Menendez <amenendez15@usf.edu>
2025-09-19 15:45:49 -0400

    making sure everything is good

--- cc_4.js
+++ cc_4.js
@@ -3,7 +3,7 @@ let products= [
 {sku: "sku-002", name:"Blue Jeans",category:"apparel",price:2,inventory:150},
 {sku: "sku-003", name:"Steak",category:"grocerices",price:3,inventory:75},
 {sku: "sku-004", name:"Charger",category:"electronics",price:4,inventory:50},
-{sku: "sku-005", name:"Baseball Hat",category:"apparel",price:5,inventory:150}
+{sku: "sku-005", name:"Baseball",category:"balls",price:5,inventory:150}
 ];
 
 for (let product of products) // Sets up switch, creates a "box" of array above 
@@ -24,16 +24,13 @@ for (let product of products) // Sets up switch, creates a "box" of array above
             discount = 0
 }
 }
+
+extraDiscount = 0
 if (customerType === "senior")
 {
-    discount += .07 ;
+    extraDiscount += .07 ;
 } else {
     discount ;
 }
-let customers = [
-    [products[0], products[1], products[2]], 
-    [products[1], products[4], products[5]],
-    [products[2], products[3], products[4]]
-];
-for(let customer = customers.length, ${discount}, product.inventory -- 1)
+
 
